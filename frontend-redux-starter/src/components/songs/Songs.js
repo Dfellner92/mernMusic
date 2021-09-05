0d9137750ebf18255e71d8ccf6a31c4f09fe54cf
Dan Fellner <dfellner92@gmail.com>
2021-09-05 12:06:29 -0400

    made song table

--- frontend-redux-starter/src/components/songs/Songs.js
+++ frontend-redux-starter/src/components/songs/Songs.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
 const Songs = () => {
-    return <div>Songs</div>
+    return (
+        <div>
+          <h2 className="text-center">Songs</h2>  
+        
+          <table className="table table-striped .table-hover shadow text-center">
+            <thead className="bg-info table-dark">
+              <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Url</th>
+                <th scope="col">Rating</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Faded by Alan Walker</td>
+                <td>https://www.youtube.com</td>
+                <td>5</td>
+                <td>
+                    <button className="btn btn-info">Edit</button>
+                    <button className="btn btn-info">Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    );
 }
 
 export default Songs;
