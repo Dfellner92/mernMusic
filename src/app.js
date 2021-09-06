48c2e894d7e53287e92d30c7eebf00f7e9e1b4a8
Dan Fellner <dfellner92@gmail.com>
2021-09-06 17:46:51 -0400

    trying sol 1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import logger from "morgan";
 import SwaggerUi from "swagger-ui-express";
-import cors from 'cors';
+import cors from "cors";
 
 import { connect } from "./config/db";
 import { restRouter } from "./api";
-import swaggerDocument from './config/swagger.json';
+import swaggerDocument from "./config/swagger.json";
+import { Server } from "http";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -40,6 +41,12 @@ app.use((error, req, res, next) => {
       message: error.message,
     },
   });
+});
+
+// trying
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"));
 });
 
 app.listen(PORT, () => {
